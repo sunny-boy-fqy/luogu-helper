632694712751f094671c1e8fb6eb56c4952960bc
sunny-boy-fqy <fqy17107713@gmail.com>
2026-03-05 15:12:49 +0800

    allow c++ 98

--- include/qio.hpp
+++ include/qio.hpp
@@ -1,48 +1,109 @@
 /*
-Fast I/O library - Cross platform (Linux/Windows/macOS)
-Uses mmap on Linux, file mapping on Windows, and fallback to buffered I/O
-*/
+ * Fast I/O library - Cross platform (Linux/Windows/macOS)
+ * Supports C++98, C++11, and above.
+ * Automatically detects compiler version for BigInt (bint) support.
+ */
 
 #ifndef QIO_HPP
 #define QIO_HPP
 
 #include <algorithm>
-#include <cctype>
 #include <cstdio>
-#include <cstdint>
 #include <cstring>
 #include <string>
-#include <type_traits>
 #include <cmath>
 #include <sys/stat.h>
-#include "bint.hpp"
-#ifdef _WIN32
-#include <windows.h>
-#include <io.h>
+#include <iostream>
+
+// ==================================================================================
+// 编译器特性检测与大数类集成
+// ==================================================================================
+
+// 只有 G++ >= 4.8 且开启 C++11 时才尝试包含 bint.hpp
+#undef QIO_HAS_BINT
+#if defined(__GNUC__)
+    #if (__GNUC__ * 100 + __GNUC_MINOR__ >= 408) && (__cplusplus >= 201103L)
+        #define QIO_HAS_BINT 1
+    #endif
+#elif defined(_MSC_VER)
+    #if _MSC_VER >= 1900 // VS 2015+
+        #define QIO_HAS_BINT 1
+    #endif
+#endif
+
+// 如果用户手动定义了 QIO_NO_BINT，则强制关闭
+#ifdef QIO_NO_BINT
+    #undef QIO_HAS_BINT
+#endif
+
+#ifdef QIO_HAS_BINT
+    #include "bint.hpp"
+#endif
+
+// ==================================================================================
+// 兼容性类型定义 (针对 C++98)
+// ==================================================================================
+#if defined(_MSC_VER) && _MSC_VER < 1600
+    typedef unsigned __int32 uint32_t;
+    typedef unsigned __int16 uint16_t;
+    typedef unsigned __int64 uint64_t;
+    typedef __int64          int64_t;
 #else
-#include <unistd.h>
-#ifdef __linux__
-#include <sys/mman.h>
+    #include <stdint.h>
 #endif
+
+#ifdef _WIN32
+    #include <windows.h>
+    #include <io.h>
+    #define Q_ISATTY _isatty
+    #define Q_FILENO _fileno
+#else
+    #include <unistd.h>
+    #define Q_ISATTY isatty
+    #define Q_FILENO fileno
+    #ifdef __linux__
+        #include <sys/mman.h>
+    #endif
 #endif
 
 // ==================================================================================
-// 辅助组件
+// 辅助组件：类型识别与查表
 // ==================================================================================
 
-// 输出 2 位查表
+// 简单的类型识别 (替代 C++11 type_traits)
+template<typename T> struct QIsIntegral { static const bool value = false; };
+template<> struct QIsIntegral<int> { static const bool value = true; };
+template<> struct QIsIntegral<long> { static const bool value = true; };
+template<> struct QIsIntegral<short> { static const bool value = true; };
+template<> struct QIsIntegral<unsigned int> { static const bool value = true; };
+template<> struct QIsIntegral<unsigned long> { static const bool value = true; };
+template<> struct QIsIntegral<signed char> { static const bool value = true; };
+template<> struct QIsIntegral<unsigned char> { static const bool value = true; };
+#if defined(__GNUC__) || defined(_MSC_VER) // 支持 long long
+template<> struct QIsIntegral<long long> { static const bool value = true; };
+template<> struct QIsIntegral<unsigned long long> { static const bool value = true; };
+#endif
+
+template<typename T> struct QIsSigned { static const bool value = false; };
+template<> struct QIsSigned<int> { static const bool value = true; };
+template<> struct QIsSigned<long> { static const bool value = true; };
+template<> struct QIsSigned<short> { static const bool value = true; };
+template<> struct QIsSigned<signed char> { static const bool value = true; };
+#if defined(__GNUC__) || defined(_MSC_VER)
+template<> struct QIsSigned<long long> { static const bool value = true; };
+#endif
+
 struct OutputTable {
     char data[100][2];
-    constexpr OutputTable() : data() {
+    OutputTable() {
         for (int i = 0; i < 100; ++i) {
-            data[i][0] = (i / 10) + '0';
-            data[i][1] = (i % 10) + '0';
+            data[i][0] = (char)((i / 10) + '0');
+            data[i][1] = (char)((i % 10) + '0');
         }
     }
 };
-static constexpr OutputTable output_table;
+static const OutputTable output_table;
 
-// 输入 2 位查表
 struct InputPre {
     uint32_t m_data[0x10000];
     InputPre() {
@@ -54,15 +115,14 @@ struct InputPre {
         }
     }
 };
-static InputPre input_pre;
+static const InputPre input_pre;
 
 // ==================================================================================
-// QInStream: 极速输入 (mmap + 查表)
-// 注意：仅在文件重定向时启用优化，终端模式下使用标准 cin
+// QInStream
 // ==================================================================================
 class QInStream {
 private:
-    static constexpr size_t LARGE_BUFFER_SIZE = 1 << 26; // 64MB
+    static const size_t BUFFER_SIZE = 1 << 26; // 64MB
     char *m_p, *m_c, *m_end;
     bool m_is_mmap;
     FILE *m_file;
@@ -70,7 +130,7 @@ private:
     
     void refill() {
         if (m_file && !m_is_mmap && m_use_fast_io) {
-            size_t n = fread(m_p, 1, LARGE_BUFFER_SIZE, m_file);
+            size_t n = fread(m_p, 1, BUFFER_SIZE, m_file);
             m_p[n] = '\0';
             m_c = m_p;
             m_end = m_p + n;
@@ -78,17 +138,9 @@ private:
     }
 
 public:
-    QInStream(FILE *file = stdin) : m_p(nullptr), m_c(nullptr), m_end(nullptr), m_is_mmap(false), m_file(nullptr), m_use_fast_io(false) {
-        bool is_tty = false;
-#ifdef _WIN32
-        int fd = _fileno(file);
-        is_tty = _isatty(fd) != 0;
-#else
-        int fd = fileno(file);
-        is_tty = isatty(fd) != 0;
-#endif
-        
-        if (!is_tty) {
+    QInStream(FILE *file = stdin) : m_p(NULL), m_c(NULL), m_end(NULL), m_is_mmap(false), m_file(NULL), m_use_fast_io(false) {
+        int fd = Q_FILENO(file);
+        if (!Q_ISATTY(fd)) {
             struct stat st;
             if (fstat(fd, &st) == 0 && st.st_size > 0) {
 #ifdef _WIN32
@@ -99,32 +151,23 @@ public:
                         m_p = (char *)MapViewOfFileEx(hMap, FILE_MAP_READ, 0, 0, 0, NULL);
                         CloseHandle(hMap);
                         if (m_p != NULL) {
-                            m_is_mmap = true;
-                            m_c = m_p;
-                            m_end = m_p + st.st_size;
-                            m_use_fast_io = true;
-                            return;
+                            m_is_mmap = true; m_c = m_p; m_end = m_p + st.st_size;
+                            m_use_fast_io = true; return;
                         }
                     }
                 }
 #elif defined(__linux__)
-                m_p = (char *)mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+                m_p = (char *)mmap(NULL, (size_t)st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
                 if (m_p != MAP_FAILED) {
-                    m_is_mmap = true;
-                    m_c = m_p;
-                    m_end = m_p + st.st_size;
-                    m_use_fast_io = true;
-                    return;
+                    m_is_mmap = true; m_c = m_p; m_end = m_p + st.st_size;
+                    m_use_fast_io = true; return;
                 }
 #endif
             }
-            
             m_file = file;
-            m_p = new char[LARGE_BUFFER_SIZE + 1];
-            size_t n = fread(m_p, 1, LARGE_BUFFER_SIZE, file);
-            m_p[n] = '\0';
-            m_c = m_p;
-            m_end = m_p + n;
+            m_p = new char[BUFFER_SIZE + 1];
+            size_t n = fread(m_p, 1, BUFFER_SIZE, file);
+            m_p[n] = '\0'; m_c = m_p; m_end = m_p + n;
             m_use_fast_io = true;
         }
     }
@@ -132,278 +175,155 @@ public:
     ~QInStream() {
         if (m_p) {
 #ifdef _WIN32
-            if (m_is_mmap) UnmapViewOfFile(m_p);
-            else
+            if (m_is_mmap) UnmapViewOfFile(m_p); else delete[] m_p;
 #elif defined(__linux__)
-            if (m_is_mmap) munmap(m_p, static_cast<size_t>(m_end - m_p));
-            else
-#endif
+            if (m_is_mmap) munmap(m_p, (size_t)(m_end - m_p)); else delete[] m_p;
+#else
             delete[] m_p;
+#endif
         }
     }
 
-    template <typename Tp, typename std::enable_if<std::is_integral<Tp>::value>::type * = nullptr>
+    template <typename Tp>
     QInStream &operator>>(Tp &x) {
-        if (!m_use_fast_io) {
-            std::cin >> x;
-            return *this;
-        }
-        x = 0;
-        if (m_c >= m_end) {
-            refill();
-        }
-        char *c = m_c;
-        char *end = m_end;
-        while (c < end && ((unsigned char)*c < '0' || (unsigned char)*c > '9') && *c != '-') ++c;
+        if (!m_use_fast_io) { std::cin >> x; return *this; }
+        x = 0; if (m_c >= m_end) refill();
+        char *c = m_c, *end = m_end;
+        while (c < end && (*c < '0' || *c > '9') && *c != '-') ++c;
         if (c >= end) { m_c = c; return *this; }
         bool neg = false;
-        if (std::is_signed<Tp>::value && *c == '-') { neg = true; ++c; }
-        while (c < end && (unsigned char)*c >= '0' && (unsigned char)*c <= '9') {
+        if (QIsSigned<Tp>::value && *c == '-') { neg = true; ++c; }
+        while (c < end && *c >= '0' && *c <= '9') {
             if (c + 1 < end) {
                 uint16_t val = (uint16_t)((unsigned char)c[0] | ((unsigned char)c[1] << 8));
                 uint32_t res = input_pre.m_data[val];
-                if (res != 0xFFFFFFFFu) {
-                    x = x * 100 + res;
-                    c += 2;
-                    continue;
-                }
+                if (res != 0xFFFFFFFFu) { x = x * 100 + res; c += 2; continue; }
             }
-            x = x * 10 + ((unsigned char)*c++ - '0');
+            x = x * 10 + (*c++ - '0');
         }
-        if (neg) x = -x;
-        m_c = c;
-        return *this;
+        if (neg) x = -x; m_c = c; return *this;
     }
 
     QInStream &operator>>(char &x) {
-        if (!m_use_fast_io) {
-            std::cin >> x;
-            return *this;
-        }
-        if (m_c >= m_end) {
-            refill();
-        }
+        if (!m_use_fast_io) { std::cin >> x; return *this; }
+        if (m_c >= m_end) refill();
         while (m_c < m_end && (unsigned char)*m_c <= ' ') m_c++;
         if (m_c < m_end) x = *m_c++;
         return *this;
     }
 
     QInStream &operator>>(std::string &x) {
-        if (!m_use_fast_io) {
-            std::cin >> x;
-            return *this;
-        }
-        x.clear();
-        if (m_c >= m_end) {
-            refill();
-        }
-        char *c = m_c;
-        char *end = m_end;
+        if (!m_use_fast_io) { std::cin >> x; return *this; }
+        x.clear(); if (m_c >= m_end) refill();
+        char *c = m_c, *end = m_end;
         while (c < end && (unsigned char)*c <= ' ') ++c;
         if (c >= end) { m_c = c; return *this; }
         char *start = c;
         while (c < end && (unsigned char)*c > ' ') ++c;
-        x.assign(start, c - start);
-        m_c = c;
-        return *this;
+        x.assign(start, (size_t)(c - start));
+        m_c = c; return *this;
     }
 
-    QInStream &operator>>(double &x) {
-        if (!m_use_fast_io) {
-            std::cin >> x;
-            return *this;
-        }
-        if (m_c >= m_end) {
-            refill();
-        }
-        double t = 0.0;
-        int sign = 1;
-        char *c = m_c;
-        char *end = m_end;
-        while (c < end && !((unsigned char)*c >= '0' && (unsigned char)*c <= '9') && *c != '-' && *c != '+' && *c != '.') ++c;
-        if (c >= end) { x = 0.0; m_c = c; return *this; }
-        if (*c == '-') { sign = -1; ++c; }
-        else if (*c == '+') { ++c; }
-
-        bool any = false;
-        while (c < end && (unsigned char)*c >= '0' && (unsigned char)*c <= '9') { any = true; t = t * 10.0 + ((unsigned char)*c++ - '0'); }
-        if (c < end && *c == '.') {
-            ++c;
-            double frac = 0.1;
-            while (c < end && (unsigned char)*c >= '0' && (unsigned char)*c <= '9') { any = true; t += ((unsigned char)*c++ - '0') * frac; frac *= 0.1; }
-        }
-
-        if (!any) { x = 0.0; m_c = c; return *this; }
-
-        if (c < end && (*c == 'e' || *c == 'E')) {
-            ++c;
-            int exp_sign = 1;
-            if (c < end && (*c == '+' || *c == '-')) { if (*c == '-') exp_sign = -1; ++c; }
-            int exp = 0;
-            while (c < end && (unsigned char)*c >= '0' && (unsigned char)*c <= '9') { exp = exp * 10 + ((unsigned char)*c++ - '0'); }
-            if (exp != 0) {
-                int e = exp_sign * exp;
-                double p = 1.0;
-                double base = 10.0;
-                int ae = e < 0 ? -e : e;
-                while (ae) {
-                    if (ae & 1) p *= base;
-                    base *= base;
-                    ae >>= 1;
-                }
-                if (e < 0) t /= p; else t *= p;
-            }
-        }
-
-        x = t * sign;
-        m_c = c;
-        return *this;
-    }
+#ifdef QIO_HAS_BINT
     QInStream &operator>>(bint &x) {
-        if (!m_use_fast_io) {
-            std::cin >> x;
-            return *this;
-        }
-        std::string value;
-        *this >> value;
-        if (!value.empty()) x = value;
-        return *this;
+        std::string s; *this >> s; if(!s.empty()) x = s; return *this;
     }
     QInStream &operator>>(ubint &x) {
-        if (!m_use_fast_io) {
-            std::cin >> x;
-            return *this;
-        }
-        std::string value;
-        *this >> value;
-        if (!value.empty()) x = value;
-        return *this;
+        std::string s; *this >> s; if(!s.empty()) x = s; return *this;
     }
+#endif
+
     void tie(void*) {}
 };
 
 // ==================================================================================
-// QOutStream: 极速输出 (大缓冲 + 2位查表)
+// QOutStream
 // ==================================================================================
 class QOutStream {
 private:
-    static constexpr size_t OUTPUT_BUFFER_SIZE = 1 << 20; // 1MB
+    static const size_t BUFFER_SIZE = 1 << 20; // 1MB
     FILE *m_file;
-    char m_buf[OUTPUT_BUFFER_SIZE], *m_c, *m_end;
+    char m_buf[BUFFER_SIZE], *m_c, *m_end;
 
 public:
     QOutStream(FILE *file = stdout) : m_file(file) {
-        m_c = m_buf;
-        m_end = m_buf + OUTPUT_BUFFER_SIZE;
+        m_c = m_buf; m_end = m_buf + BUFFER_SIZE;
     }
-
     ~QOutStream() { flush(); }
 
     void flush() {
         if (m_c != m_buf) {
-            fwrite(m_buf, 1, m_c - m_buf, m_file);
+            fwrite(m_buf, 1, (size_t)(m_c - m_buf), m_file);
             m_c = m_buf;
         }
     }
 
-    inline void put(char x) {
-        if (m_c == m_end) flush();
-        *m_c++ = x;
-    }
+    inline void put(char x) { if (m_c == m_end) flush(); *m_c++ = x; }
 
     QOutStream &operator<<(char x) { put(x); return *this; }
-
     QOutStream &operator<<(const char *s) {
         if (!s) return *this;
         size_t len = strlen(s);
-        size_t avail = static_cast<size_t>(m_end - m_c);
-        if (len <= avail) {
-            memcpy(m_c, s, len);
-            m_c += len;
-        } else {
-            const char *p = s;
-            while (*p) { if (m_c == m_end) flush(); *m_c++ = *p++; }
-        }
+        if (len <= (size_t)(m_end - m_c)) { memcpy(m_c, s, len); m_c += len; }
+        else { while (*s) put(*s++); }
         return *this;
     }
     
     QOutStream &operator<<(const std::string &s) {
         size_t len = s.size();
-        size_t avail = static_cast<size_t>(m_end - m_c);
-        if (len <= avail) {
-            memcpy(m_c, s.data(), len);
-            m_c += len;
-        } else {
-            for (char c : s) put(c);
-        }
+        if (len <= (size_t)(m_end - m_c)) { memcpy(m_c, s.data(), len); m_c += len; }
+        else { for (size_t i = 0; i < len; ++i) put(s[i]); }
         return *this;
     }
 
-    template <typename Tp, typename std::enable_if<std::is_integral<Tp>::value>::type * = nullptr>
+    template <typename Tp>
     QOutStream &operator<<(Tp x) {
+        if (!QIsIntegral<Tp>::value) return *this;
         if (m_end - m_c < 24) flush();
         if (x == 0) { put('0'); return *this; }
         uint64_t val;
-        if (std::is_signed<Tp>::value && x < 0) {
+        if (QIsSigned<Tp>::value && x < 0) {
             put('-');
-            int64_t sx = static_cast<int64_t>(x);
-            uint64_t u = static_cast<uint64_t>(-(sx + 1)) + 1; // safe for INT64_MIN
-            val = u;
-        } else {
-            val = static_cast<uint64_t>(x);
-        }
+            val = (uint64_t)-(x + 1) + 1;
+        } else val = (uint64_t)x;
         
-        char tmp[24];
-        int len = 0;
+        char tmp[24]; int len = 0;
         while (val >= 100) {
-            int idx = val % 100;
-            val /= 100;
+            int idx = (int)(val % 100); val /= 100;
             tmp[len++] = output_table.data[idx][1];
             tmp[len++] = output_table.data[idx][0];
         }
         if (val >= 10) {
-            tmp[len++] = output_table.data[val][1];
-            tmp[len++] = output_table.data[val][0];
-        } else {
-            tmp[len++] = (char)(val + '0');
-        }
+            tmp[len++] = output_table.data[(int)val][1];
+            tmp[len++] = output_table.data[(int)val][0];
+        } else tmp[len++] = (char)(val + '0');
         while (len--) put(tmp[len]);
         return *this;
     }
 
-    QOutStream &operator<<(double x) {
-        char tmp[40];
-        int len = sprintf(tmp, "%.10f", x); 
-        if (m_end - m_c < len) flush();
-        memcpy(m_c, tmp, len);
-        m_c += len;
-        return *this;
+#ifdef QIO_HAS_BINT
+    QOutStream &operator<<(const bint &x) {
+        return *this << static_cast<const char*>(x);
     }
-    
-    QOutStream &operator<<(bint x) {
-        const char* value = static_cast<const char*>(x);
-        return *this << value;
+    QOutStream &operator<<(const ubint &x) {
+        return *this << static_cast<const char*>(x);
     }
-    
+#endif
+
     void tie(void*) {}
 };
 
 // ==================================================================================
-// 全局实例与宏
+// 全局实例
 // ==================================================================================
-static QInStream qin;
-static QOutStream qout;
+static QInStream qin(stdin);
+static QOutStream qout(stdout);
 static const char qendl = '\n';
 
 #ifndef QIO_NO_OVERRIDE
-#define cin qin
-#define cout qout
-#define endl qendl
+    #define cin qin
+    #define cout qout
+    #define endl qendl
 #endif
-
-#define ios _patched_ios
-namespace _patched_ios {
-    inline void sync_with_stdio(bool) {}
-}
 
 #endif // QIO_HPP
